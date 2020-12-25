9416844849d4ad4bca6e8b1611b2bf832ea44c3d
Laimonas Turauskas <laimiux@gmail.com>
2020-12-25 14:22:08 -0800

    Convert tests.

--- lce/src/main/java/com/laimiux/lce/Convert.kt
+++ lce/src/main/java/com/laimiux/lce/Convert.kt
@@ -1,13 +1,5 @@
 package com.laimiux.lce
 
-fun Type.Loading<Unit>.asUnitLoading(): Type.Loading.UnitType {
-    return Type.Loading.UnitType
-}
-
-fun Type.Error<Throwable>.asThrowableError(): Type.Error.ThrowableType {
-    return this as Type.Error.ThrowableType
-}
-
 fun <ContentT> LCE<Unit, ContentT, Throwable>.asUCT(): UCT<ContentT> {
     return foldTypes(
         onLoading = { it.asUnitLoading() },
@@ -16,7 +8,7 @@ fun <ContentT> LCE<Unit, ContentT, Throwable>.asUCT(): UCT<ContentT> {
     )
 }
 
-inline fun <C> UCT<C>.asLCE(): LCE<Unit, C, Throwable> {
+fun <C> UCT<C>.asLCE(): LCE<Unit, C, Throwable> {
     return foldTypes(
         onLoading = { it },
         onContent = { it },
@@ -24,18 +16,66 @@ inline fun <C> UCT<C>.asLCE(): LCE<Unit, C, Throwable> {
     )
 }
 
-inline fun <ContentT> CE<ContentT, Throwable>.asUCT(): UCT<ContentT> {
-    return when(val type = asLceType()) {
-        is Type.Content -> type
-        is Type.Error.ThrowableType -> type
-        else -> throw IllegalStateException("this should not happen: $type")
-    }
+fun <ContentT, ErrorT> CE<ContentT, ErrorT>.asLCE(): LCE<Nothing, ContentT, ErrorT> {
+    return foldTypes(
+        onContent = { it },
+        onError = { it }
+    )
 }
 
-inline fun <ContentT> CE<ContentT, Throwable>.asCT(): CT<ContentT> {
-    return when(val type = asLceType()) {
-        is Type.Content -> type
-        is Type.Error.ThrowableType -> type
-        else -> throw IllegalStateException("this should not happen: $type")
-    }
+fun <ContentT> CE<ContentT, Throwable>.asUCT(): UCT<ContentT> {
+    return foldTypes(
+        onContent = { it },
+        onError = { it.asThrowableError() }
+    )
+}
+
+fun <ContentT> CE<ContentT, Throwable>.asCT(): CT<ContentT> {
+    return foldTypes(
+        onContent = { it },
+        onError = { it.asThrowableError() }
+    )
+}
+
+fun <ContentT> CT<ContentT>.asCE(): CE<ContentT, Throwable> {
+    return foldTypes(
+        onContent = { it },
+        onError = { it }
+    )
+}
+
+fun <ContentT> CT<ContentT>.asUCT(): UCT<ContentT> {
+    return foldTypes(
+        onContent = { it },
+        onError = { it }
+    )
+}
+
+fun <ContentT> CT<ContentT>.asLCE(): LCE<Nothing, ContentT, Throwable> {
+    return foldTypes(
+        onContent = { it },
+        onError = { it }
+    )
+}
+
+fun <ContentT> UC<ContentT>.asLCE(): LCE<Unit, ContentT, Nothing> {
+    return foldTypes(
+        onLoading = { it },
+        onContent = { it }
+    )
+}
+
+fun <ContentT> UC<ContentT>.asUCT(): UCT<ContentT> {
+    return foldTypes(
+        onLoading = { it },
+        onContent = { it }
+    )
+}
+
+internal fun Type.Loading<Unit>.asUnitLoading(): Type.Loading.UnitType {
+    return Type.Loading.UnitType
+}
+
+internal fun Type.Error<Throwable>.asThrowableError(): Type.Error.ThrowableType {
+    return this as Type.Error.ThrowableType
 }
