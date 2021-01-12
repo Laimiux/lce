0e5eeca03c9c2fffb6bbb1ba31d0cc4d3546cf0b
Laimonas Turauskas <laimiux@gmail.com>
2021-01-11 18:42:35 -0800

    Add LC.merge extension. (#19)

--- lce/src/test/java/com/laimiux/lce/MergeTest.kt
+++ lce/src/test/java/com/laimiux/lce/MergeTest.kt
@@ -5,6 +5,21 @@ import org.junit.Test
 
 class MergeTest {
 
+    @Test fun `LC loading merge returns loading`() {
+        val result = LC.loading().merge(LC.content(0))
+        assertThat(result).isEqualTo(LC.loading())
+    }
+
+    @Test fun `LC content returns loading when other LC is loading`() {
+        val result = LC.content(0).merge(LC.loading())
+        assertThat(result).isEqualTo(LC.loading())
+    }
+
+    @Test fun `LC content returns merged content when other LC is content`() {
+        val result = LC.content(0).merge(LC.content(""))
+        assertThat(result).isEqualTo(LC.content(Pair(0, "")))
+    }
+
     @Test fun `UC loading merge returns loading`() {
         val result = UC.loading().merge(UC.content(0))
         assertThat(result).isEqualTo(UC.loading())
