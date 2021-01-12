6deb50a6611fc9abc80aadbc5fddb5c1b583802f
Laimonas Turauskas <laimiux@gmail.com>
2021-01-11 18:39:44 -0800

    Add LC.merge extension.

--- lce/src/main/java/com/laimiux/lce/Merge.kt
+++ lce/src/main/java/com/laimiux/lce/Merge.kt
@@ -2,6 +2,28 @@ package com.laimiux.lce
 
 
 /**
+ * Merges two [LC] objects into one.
+ */
+fun <L, C, C2> LC<L, C>.merge(other: LC<L, C2>): LC<L, Pair<C, C2>> {
+    return merge(other) { first, second -> Pair(first, second) }
+}
+
+/**
+ * Merges two [LC] objects into one.
+ */
+inline fun <L, C, C2, T> LC<L, C>.merge(
+    other: LC<L, C2>,
+    crossinline merge: (C, C2) -> T
+): LC<L, T> {
+    return flatMapContent { first ->
+        other.flatMapContent { second ->
+            LC.content(merge(first, second))
+        }
+    }
+}
+
+
+/**
  * Merges two [UC] objects into one.
  */
 fun <C, C2> UC<C>.merge(other: UC<C2>): UC<Pair<C, C2>> {
