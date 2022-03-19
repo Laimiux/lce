94c64daf262ec9a50a3d2e54e01e35373c1dc271
Laimonas Turauskas <laimiux@gmail.com>
2022-03-18 17:40:05 -0700

    Merge with 3 and 4 events. (#35)

--- lce/src/main/java/com/laimiux/lce/Merge.kt
+++ lce/src/main/java/com/laimiux/lce/Merge.kt
@@ -13,6 +13,33 @@ inline fun <C, C2, E, T> UCE<C, E>.merge(
 }
 
 /**
+ * Merges three [UCE] objects into one.
+ */
+inline fun <C, C2, C3, E, T> UCE<C, E>.merge(
+    second: UCE<C2, E>,
+    third: UCE<C3, E>,
+    crossinline merge: (C, C2, C3) -> T
+): UCE<T, E> {
+    return takeFirstError(second, third) { firstUC, secondUC, thirdUC ->
+        firstUC.merge(secondUC, thirdUC, merge).asUCE()
+    }
+}
+
+/**
+ * Merges three [UCE] objects into one.
+ */
+inline fun <C, C2, C3, C4, E, T> UCE<C, E>.merge(
+    second: UCE<C2, E>,
+    third: UCE<C3, E>,
+    fourth: UCE<C4, E>,
+    crossinline merge: (C, C2, C3, C4) -> T
+): UCE<T, E> {
+    return takeFirstError(second, third, fourth) { firstUC, secondUC, thirdUC, fourthUC ->
+        firstUC.merge(secondUC, thirdUC, fourthUC, merge).asUCE()
+    }
+}
+
+/**
  * Merges two [UCE] objects into one.
  */
 fun <C, C2, E> UCE<C, E>.merge(other: UCE<C2, E>): UCE<Pair<C, C2>, E> {
@@ -25,11 +52,38 @@ fun <C, C2, E> UCE<C, E>.merge(other: UCE<C2, E>): UCE<Pair<C, C2>, E> {
  * Merges two [UCT] objects into one.
  */
 inline fun <C, C2, T> UCT<C>.merge(
-    other: UCT<C2>,
+    second: UCT<C2>,
     crossinline merge: (C, C2) -> T
 ): UCT<T> {
-    return takeFirstError(other) { first, second ->
-        first.merge(second, merge).asUCT()
+    return takeFirstError(second) { firstUC, secondUC ->
+        firstUC.merge(secondUC, merge).asUCT()
+    }
+}
+
+/**
+ * Merges three [UCT] objects into one.
+ */
+inline fun <C, C2, C3, T> UCT<C>.merge(
+    second: UCT<C2>,
+    third: UCT<C3>,
+    crossinline merge: (C, C2, C3) -> T
+): UCT<T> {
+    return takeFirstError(second, third) { firstUC, secondUC, thirdUC ->
+        firstUC.merge(secondUC, thirdUC, merge).asUCT()
+    }
+}
+
+/**
+ * Merges four [UCT] objects into one.
+ */
+inline fun <C, C2, C3, C4, T> UCT<C>.merge(
+    second: UCT<C2>,
+    third: UCT<C3>,
+    fourth: UCT<C4>,
+    crossinline merge: (C, C2, C3, C4) -> T
+): UCT<T> {
+    return takeFirstError(second, third, fourth) { firstUC, secondUC, thirdUC, fourthUC ->
+        firstUC.merge(secondUC, thirdUC, fourthUC, merge).asUCT()
     }
 }
 
@@ -63,6 +117,43 @@ inline fun <L, C, C2, T> LC<L, C>.merge(
     }
 }
 
+/**
+ * Merges three [LC] objects into one.
+ */
+inline fun <L, C, C2, C3, T> LC<L, C>.merge(
+    second: LC<L, C2>,
+    third: LC<L, C3>,
+    crossinline merge: (C, C2, C3) -> T
+): LC<L, T> {
+    return flatMapContent { firstContent ->
+        second.flatMapContent { secondContent ->
+            third.flatMapContent { thirdContent ->
+                LC.content(merge(firstContent, secondContent, thirdContent))
+            }
+        }
+    }
+}
+
+/**
+ * Merges three [LC] objects into one.
+ */
+inline fun <L, C, C2, C3, C4, T> LC<L, C>.merge(
+    second: LC<L, C2>,
+    third: LC<L, C3>,
+    fourth: LC<L, C4>,
+    crossinline merge: (C, C2, C3, C4) -> T
+): LC<L, T> {
+    return flatMapContent { firstContent ->
+        second.flatMapContent { secondContent ->
+            third.flatMapContent { thirdContent ->
+                fourth.flatMapContent { fourthContent ->
+                    LC.content(merge(firstContent, secondContent, thirdContent, fourthContent))
+                }
+            }
+        }
+    }
+}
+
 
 /**
  * Merges two [UC] objects into one.
@@ -81,6 +172,43 @@ inline fun <C, C2, T> UC<C>.merge(
     return flatMapContent { first ->
         other.flatMapContent { second ->
             UC.content(merge(first, second))
+        }
+    }
+}
+
+/**
+ * Merges three [UC] objects into one.
+ */
+inline fun <C, C2, C3, T> UC<C>.merge(
+    second: UC<C2>,
+    third: UC<C3>,
+    crossinline merge: (C, C2, C3) -> T
+): UC<T> {
+    return flatMapContent { firstContent ->
+        second.flatMapContent { secondContent ->
+            third.flatMapContent { thirdContent ->
+                UC.content(merge(firstContent, secondContent, thirdContent))
+            }
+        }
+    }
+}
+
+/**
+ * Merges four [UC] objects into one.
+ */
+inline fun <C, C2, C3, C4, T> UC<C>.merge(
+    second: UC<C2>,
+    third: UC<C3>,
+    fourth: UC<C4>,
+    crossinline merge: (C, C2, C3, C4) -> T
+): UC<T> {
+    return flatMapContent { firstContent ->
+        second.flatMapContent { secondContent ->
+            third.flatMapContent { thirdContent ->
+                fourth.flatMapContent { fourthContent ->
+                    UC.content(merge(firstContent, secondContent, thirdContent, fourthContent))
+                }
+            }
         }
     }
 }
